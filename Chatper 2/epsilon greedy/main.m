59574fd4bbe74ee1a425f97daf80755caf28e979
Aamanku <55757512+aamanku@users.noreply.github.com>
2020-06-11 22:31:25 -0400

    Update main.m

--- Chatper 2/epsilon greedy/main.m
+++ Chatper 2/epsilon greedy/main.m
@@ -1,18 +1,23 @@
 clear all
 Rplot=zeros([1000 1]);
-epsilon=0.1;
-hold on;
+optiplot=zeros([1000 1]);
+epsilon=0.01;
 for sample=1:2000
     NA=ones([10 1]);
-    mu=zeros([10 1]);
+    mu=10*ones([10 1]);
     for timestep=1:1000
         A=SelectAction(mu,epsilon);
         R=GetReward(A);
         mu(A)=mu(A)+(R-mu(A))/NA(A);
         NA(A)=NA(A)+1;
         Rplot(timestep)=Rplot(timestep)+(R-Rplot(timestep))/sample;
-    end
-    
+        optiplot(timestep)=optiplot(timestep)+(A==3);
+    end   
 end
 plot(1:1000,Rplot)
 ylim([-2 2])
+figure
+plot(1:1000,optiplot/20)
+ylim([0 100])
+Rplot=zeros([1000 1]);
+epsilon=0.1;
